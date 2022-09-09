80677a8a12ac6fb1b60453e5b8cf75e1204ae789
Lomidrevo2 <mat.krajnak@gmail.com>
2022-09-09 16:16:49 +0200

    b5

--- zkouska.py
+++ zkouska.py
@@ -1,6 +1,6 @@
 name = input()
+age = int(input())
 
-
-def future(name):
-    print('You will have a long life ' + name)
+def future(name, age):
+    print('You will have a long life ' + name , age)
 
